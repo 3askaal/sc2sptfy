03bc23f5bfb695159c131154b40e039cd6ad0db9
Bas Kaal <3askaal@gmail.com>
2023-10-05 11:03:49 +0200

    init server

--- client/src/components/logo.tsx
+++ client/src/components/logo.tsx
@@ -5,7 +5,6 @@ const SLogoSc = s.div(({ theme, color }: any) => ({
   background: theme.colors.scGradient,
   '-webkit-background-clip': 'text',
   '-webkit-text-fill-color': 'transparent',
-  userSelect: 'none',
   marginRight: '.125rem'
 }))
 
@@ -14,13 +13,12 @@ const SLogoSptfy = s.div(({ theme, color }: any) => ({
   background: theme.colors.sptfyGradient,
   '-webkit-background-clip': 'text',
   '-webkit-text-fill-color': 'transparent',
-  userSelect: 'none',
   marginLeft: '.125rem'
 }))
 
 export function Logo({ small }: any) {
   return (
-    <Title s={{ fontFamily: 'logo', fontSize: small ? '2.25rem' : '3.5rem' }}>
+    <Title s={{ fontFamily: 'logo', fontSize: small ? '2.25rem' : '3.5rem', userSelect: 'none' }}>
       <SLogoSc color="sc">sc</SLogoSc>2<SLogoSptfy color="sptfy">sptfy</SLogoSptfy>
     </Title>
   )
