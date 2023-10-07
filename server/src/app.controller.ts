f9c1f6d7bae3614fdcb85f520db1347c97c6c340
Bas Kaal <3askaal@gmail.com>
2023-10-07 02:35:17 +0200

    progress on tracklist generation logic

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { AppService } from './app.service';
-import promiseSequential from 'promise-sequential';
-import { CONFIG } from 'config';
 import { SpotifyApi } from '@spotify/web-api-ts-sdk';
+import sequential from 'promise-sequential';
+import to from 'await-to-js';
+import { AppService } from './app.service';
+import { CONFIG } from '../config';
 
 @Controller()
 export class AppController {
@@ -14,31 +15,26 @@ export class AppController {
   }
 
   @Get('generate/:id')
-  async generate(@Param() params): Promise<any> {
-    const sdk = SpotifyApi.withClientCredentials(
-      CONFIG.SPTFY.CLIENT_ID as string,
-      CONFIG.SPTFY.CLIENT_SECRET as string,
-    );
+  async generate(@Param() { userId, scUser, accessToken }): Promise<any> {
+    const sdk = SpotifyApi.withAccessToken(CONFIG.SPTFY.CLIENT_ID, accessToken);
 
-    const items = (await this.appService.getFavorites(params.id))
+    const items = (await this.appService.getFavorites(scUser.id))
       .filter(({ kind }) => kind === 'track')
       .filter(({ duration }) => Math.floor(duration / 60000) < 20);
 
-    const tracks = await promiseSequential(
-      items.slice(0, 5).map((item) => async () => {
-        const title = item.title;
+    const tracks = await sequential(
+      items.slice(0, 50).map((item) => async () => {
+        const [sptfySearchErr, sptfySearchSuccess] = await to(
+          sdk.search(item.title, ['track']),
+        );
 
-        console.log('title: ', title);
-
-        const results = await sdk.search(title, ['track']);
-
-        console.log('results: ', results.tracks.items.length);
+        if (sptfySearchErr) throw sptfySearchErr;
 
         const stringifiedItem = JSON.stringify(
           Object.values(item),
         ).toLowerCase();
 
-        const matches = results.tracks.items.filter((result) => {
+        const matches = sptfySearchSuccess.tracks.items.filter((result) => {
           const criticalValues = [
             result.name,
             ...result.artists.map(({ name }) => name),
@@ -49,8 +45,6 @@ export class AppController {
           );
         });
 
-        console.log('matches: ', matches.length);
-
         return matches.map(({ id, name, artists }) => ({
           id,
           name,
@@ -58,6 +52,28 @@ export class AppController {
         }));
       }),
     );
+
+    const tracklist = tracks.map(({ id }) => id);
+
+    const [createPlaylistErr, createPlaylistSuccess] = await to(
+      sdk.playlists.createPlaylist(userId, {
+        name: scUser.name,
+        description: 'Generated with http://sc2sptfy.vercel.app',
+        public: false,
+      }),
+    );
+
+    if (createPlaylistErr) throw createPlaylistErr;
+
+    const spotifyId = createPlaylistSuccess.id;
+
+    const [addItemsToPlaylistErr] = await to(
+      sdk.playlists.addItemsToPlaylist(spotifyId, tracklist),
+    );
+
+    if (addItemsToPlaylistErr) {
+      throw addItemsToPlaylistErr;
+    }
 
     return tracks;
   }
