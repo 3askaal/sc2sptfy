b37f22e1a07070889ecaa7459824c528799d884b
Bas Kaal <3askaal@gmail.com>
2023-10-15 18:08:50 +0200

    clean up

--- client/src/pages/status/[id].tsx
+++ client/src/pages/status/[id].tsx
@@ -5,22 +5,22 @@ import { THEME } from '@/style';
 import { ChevronsRight } from 'react-feather';
 
 export default function Create() {
-  const { jobStatus, cancel } = useApi()
+  const { status, cancel } = useApi()
 
-  const isCompleted = jobStatus?.progress === 100
+  const isCompleted = status?.progress === 100
 
   return (
     <Spacer size="xl" s={{ height: '100%', alignItems: 'center', justifyContent: 'center' }}>
       <Spacer size="m" s={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-        <Text s={{ display: 'inline-flex', color: 'sc' }}>{ jobStatus?.scUser.username }</Text>
+        <Text s={{ display: 'inline-flex', color: 'sc' }}>{ status?.scUser.username }</Text>
         <ChevronsRight size={16} color={THEME.colors.primary} />
-        <Text s={{ display: 'inline-flex', color: 'sptfy' }}>{ jobStatus?.sptfyUser.display_name }</Text>
+        <Text s={{ display: 'inline-flex', color: 'sptfy' }}>{ status?.sptfyUser.display_name }</Text>
       </Spacer>
-      <Loader progress={jobStatus?.progress} />
+      <Loader progress={status?.progress} />
 
       { isCompleted ? (
         <Box df fdc aic>
-          <Link href={`https://open.spotify.com/playlist/${jobStatus.playlist.id}`} target='_blank'>
+          <Link href={`https://open.spotify.com/playlist/${status.playlist.id}`} target='_blank'>
             View playlist
           </Link>
         </Box>
