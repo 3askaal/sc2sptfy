01796266f8ca2e31579c5eb7a9989209387f5f17
Bas Kaal <3askaal@gmail.com>
2023-10-17 17:56:22 +0200

    UI improvements

--- client/src/pages/create.tsx
+++ client/src/pages/create.tsx
@@ -1,5 +1,7 @@
-import { Spacer, Input, List, ListItem } from '3oilerplate'
+import { Spacer, Input, List, ListItem, Box } from '3oilerplate'
+import { ScrollContainer } from '@/components'
 import useApi from '@/hooks/useApi'
+import Image from 'next/image'
 
 export default function Create() {
   const { users, setSearchQuery, selectedUser, setSelectedUser } = useApi()
@@ -11,17 +13,31 @@ export default function Create() {
         s={{ width: '100% !important' }}
       />
       { !!users.length && (
-        <List>
-          { users.map((user: any, index: number) => (
-            <ListItem
-              key={`user-${index}`}
-              onClick={() => setSelectedUser(user)}
-              s={{ bg: user?.id === selectedUser?.id ? 'primary' : null }}
-            >
-              { user.username }
-            </ListItem>
-          )) }
-        </List>
+        <ScrollContainer>
+          <List>
+            { users.map((user: any, index: number) => (
+              <ListItem
+                key={`user-${index}`}
+                onClick={() => setSelectedUser(user)}
+                s={{
+                  bg: user?.id === selectedUser?.id ? 'primary' : null,
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <Box s={{ overflow: 'hidden', borderRadius: '100%', mr: 'm', border: '1px solid', borderColor: 'white' }}>
+                  <Image
+                    src={user.avatar_url}
+                    width={40}
+                    height={40}
+                    alt={`${user.username.toLowerCase()}-avatar`}
+                  />
+                </Box>
+                { user.username }
+              </ListItem>
+            )) }
+          </List>
+        </ScrollContainer>
       )}
     </Spacer>
   )
